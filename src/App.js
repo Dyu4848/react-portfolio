17b6c41a45b01c0c20f8f55c9c9ac6c162b9cfcf
David Yu <dyu4848@gmail.com>
2023-02-09 15:56:04 -0500

    added file structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import{ Header, Footer} from '../src/components';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
