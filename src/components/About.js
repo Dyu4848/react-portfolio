8598fa6126d6e0f36d8e283caff89f3bc461c7e9
David Yu <dyu4848@gmail.com>
2023-03-29 18:19:01 -0400

    finished landing page and resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const About = ({setCurrentPage}) => {
-    return (
-      <div class="card d-flex justify-content-center">
-        <h1 class='card-text'>Hi My Name Is David Yu</h1>
-        <p class='card-text'>Aspiring front end web developer </p>
-      </div>
-    )}
+const About = ({ setCurrentPage }) => {
+  return (
+    <div id="info">
 
-    export default About;
+    {/* <div id="profile">
+      <p>
+        
+      </p>
+    </div> */}
+
+      <p id="hello">Hello! My name is David Yu!</p>
+      <p id="intro">
+        I am an aspiring front-end/UI developer, focusing on creating user friendly
+        applications. I have graduated from the Rutgers full stack flex coding bootcamp, and have decided to focus my time and efforts into front end development and creating UI designs that create a clean environment within web applications. My skillset is geared towards front end development including HTML, CSS, Javascript, API interactions, Bootstrap, React, and Figma. 
+      </p>
+
+      {/* <div id="skills">
+        <img id="figma" src="./images/Figma.png" alt="Figma"></img>
+        <img id="css" src="./images/css.png" alt="CSS"></img>
+        <img id="js" src="./images/JS.png" alt="JS"></img>
+        <img id="react" src="./images/React.png" alt="React"></img>
+        <img id="html" src="./images/html.png" alt="html"></img>
+         <img id="github" src="./images/github.png" alt="github"></img>
+      </div> */}
+    </div>
+  );
+};
+
+export default About;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,15 +1,123 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 function Resume() {
-    return (
-        <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..."></img>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    )}
+  return (
+    <div id="container">
+      <header id="header">
+        <p>
+          <strong id="resume-name">David Yu</strong>
+          <p>Dyu4848@gmail.com | 201-744-9150</p>
+        </p>
+      </header>
+      <br />
+      <div id="education">
+        <p>
+          <strong>Education</strong>
+        </p>
+        <hr />
+        <strong>University of Colorado, Boulder</strong>
+        <br />
+        <br />
+        <span>School of Arts and Sciences, Economics</span>
+        <span id="boulder-date">May, 2022</span>
+        <br />
+        <br />
+        <strong>Rutgers University Full Stack Coding Bootcamp</strong>
+        <br />
+        <br />
+        <span>Graduated with Certificate</span>
+        <span id="rutgers-date">February, 2023</span>
+      </div>
 
-    export default Resume;
+      <br />
+      <br />
+
+      <div className="skills">
+        <p>
+          <strong>Skills</strong>
+        </p>
+        <hr />
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>Bootstrap</li>
+          <li>React.JS</li>
+          <li>API Interactions</li>
+          <li>Github</li>
+          <li>Git</li>
+          <li>Microsoft Excel</li>
+        </ul>
+      </div>
+
+      <br />
+      <br />
+
+      <div className="experience">
+        <p>
+          <strong>Experience</strong>
+        </p>
+        <hr />
+        <span>
+          <strong>LG Electronics</strong>
+        </span>
+        <span id="lg-town">Englewood Cliffs</span>
+        <br />
+        <br />
+        <span id="claims">
+          <i>B2B Claims Coordinator</i>
+        </span>
+        <span id="lg-date">August, 2022 - November, 2022</span>
+        <ul>
+          <li>Entering new claims for customers and salesmen.</li>
+          <li>
+            Validating SPIFF, ADV, Tax, and Damage Allowance claims for
+            approval.
+          </li>
+          <li>Communicating with salesmen for supporting documentation.</li>
+          <li>
+            Creating Excel summaries and pivot tables for weekly/quarterly
+            reports.
+          </li>
+        </ul>
+        <br />
+        <span>
+          <strong>White Stone Cleaners</strong>
+        </span>
+        <span id="cleaners-town">Lake Hiawatha</span>
+        <br />
+        <br />
+        <span id="customer-service">
+          <i>Customer Service</i>
+        </span>
+        <span id="cleaners-date">March, 2019 - November, 2021</span>
+        <ul>
+          <li>Creating general spreadesheets on Excel</li>
+          <li>Answering customer phone calls and assisting in needs</li>
+          <li>Cataloging clothing items in relation with customer IDs.</li>
+        </ul>
+      </div>
+
+      <br />
+      <br />
+
+      <div className="certificates">
+        <strong>Certificates</strong>
+        <hr />
+        <ul>
+          <li>
+            <span>Rutgers Full Stack Coding Bootcamp</span>
+            <span id="bootcamp-date">February, 2023</span>
+          </li>
+          <br />
+          <li>
+            <span>FRCC Business Certificate</span>
+            <span id="frcc-date">November, 2018</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
